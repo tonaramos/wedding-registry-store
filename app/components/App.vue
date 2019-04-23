be009a1f9a0c7ca77dd8d49974e4aa2638db9798
tonaramos <gtonaramos@gmail.com>
2019-04-23 12:36:58 -0700

    Project Setup.

---
+++ app/components/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <Page>
+        <ActionBar title="D & A Wedding registry!" android:flat="true"/>
+        <TabView android:tabBackgroundColor="#53ba82"
+                 android:tabTextColor="#c4ffdf"
+                 android:selectedTabTextColor="#ffffff"
+                 androidSelectedTabHighlightColor="#ffffff">
+            <TabViewItem title="B remainding" :text="'$' +amount">
+                <GridLayout columns="*" rows="*">
+                    <Label class="Budget Remaindnig" :text="'$' +amount" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="BUY">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Welcome',
+        amount: 1000
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
